7f768a1a99d761a7d94ffd16a90df5e62034c239
nerdelt <nerdelt@gmail.com>
2018-10-01 21:43:06 -0500

    Assignment 13-1 first attempt

---
+++ CrunchyyNumbers/CrunchyNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NumberUtility;
+
+
+
+namespace CrunchyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>()
+            {
+                1, 9, 14, 27, 36, 45, 49, 52, 64, 73, 88, 92
+            };
+
+            Console.WriteLine("There is a list of numbers.\n");
+            Console.WriteLine($"The sum of even numbers in the list is: {NumberCruncher.CrunchNumbers(numbers)}\n");
+            Console.WriteLine($"Can you guess what the numbers are? \n\nlol jk\n");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CrunchyyNumbers/NumberUtility/NumberCruncher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumberUtility;
+
+namespace NumberUtility
+{
+    public class NumberCruncher
+    {
+        public static int CrunchNumbers(List<int> numbers)
+        {
+            for (int i = numbers.Count - 1; i >= 0; i--)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    numbers.Remove(numbers[i]);
+                }
+            }
+            int sum = numbers.Sum();
+
+            return sum;
+        }
+    }
+}
