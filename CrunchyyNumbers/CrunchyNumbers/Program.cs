cefe38ff88a15e8b3a9d4973caf70eb16579527a
nerdelt <nerdelt@gmail.com>
2018-10-02 21:58:09 -0500

    Assignment 13-1 Number Cruncher

--- CrunchyyNumbers/CrunchyNumbers/Program.cs
+++ CrunchyyNumbers/CrunchyNumbers/Program.cs
@@ -12,11 +12,14 @@ namespace CrunchyNumbers
     {
         static void Main(string[] args)
         {
+
+            //create list of numbers 
             List<int> numbers = new List<int>()
             {
                 1, 9, 14, 27, 36, 45, 49, 52, 64, 73, 88, 92
             };
 
+            //Call on the method in the NumberCruncher class/ NumberUtility Class Library
             Console.WriteLine("There is a list of numbers.\n");
             Console.WriteLine($"The sum of even numbers in the list is: {NumberCruncher.CrunchNumbers(numbers)}\n");
             Console.WriteLine($"Can you guess what the numbers are? \n\nlol jk\n");
--- CrunchyyNumbers/NumberUtility/NumberCruncher.cs
+++ CrunchyyNumbers/NumberUtility/NumberCruncher.cs
@@ -9,15 +9,21 @@ namespace NumberUtility
 {
     public class NumberCruncher
     {
+        //made a static method 
         public static int CrunchNumbers(List<int> numbers)
         {
+            //using a for loop when manipulating lists (and not foreach), 
+            //because foreach caused me trouble   
             for (int i = numbers.Count - 1; i >= 0; i--)
             {
+                //this is how to tell if numbers aren't even so we can remove them
                 if (numbers[i] % 2 != 0)
                 {
                     numbers.Remove(numbers[i]);
                 }
             }
+
+            //this is how to find the sum
             int sum = numbers.Sum();
 
             return sum;
